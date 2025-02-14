e3978966b38ee70a2e388fad6d3fdcab08dbceed
inferno0230 <mail@inferno0230.in>
2025-02-14 21:47:13 +0530

    Add update.sh to make life easy
    Signed-off-by: inferno0230 <mail@inferno0230.in>

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DOCKER_DIR=$(pwd)
+
+for dir in "$DOCKER_DIR"/*; do
+    if [[ -f "$dir/docker-compose.yml" ]]; then
+        echo "Updating $(basename "$dir")..."
+        (
+            cd "$dir" || exit
+            sudo docker-compose pull
+            sudo docker-compose up -d
+        )
+    fi
+done
+sudo docker system prune -a -f
+
+echo "All containers updated!"
