c1cdce50a4e70e697355a16cebc81562f0ab885c
inferno0230 <mail@inferno0230.in>
2025-12-15 08:26:02 +0000

    Wait for proper dns resolution before turning containers
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- script.sh
+++ script.sh
@@ -8,6 +8,18 @@ MAX_BACKUPS=5
 
 cd "$SCRIPT_DIR" || exit 1
 
+healthcheck() {
+    echo "Checking connectivity..."
+
+    # Retry until ping succeeds
+    until docker run --rm alpine/curl -s --connect-timeout 3 https://google.com >/dev/null; do
+        echo "Ping failed... retrying in 2 seconds"
+        sleep 5
+    done
+
+    echo "Ping OK."
+}
+
 backup() {
     echo "Stopping all containers before backup..."
     find . -name docker-compose.yml -execdir docker compose down \;
@@ -16,25 +28,23 @@ backup() {
     BACKUP_PATH="$BACKUP_DIR/$BACKUP_NAME"
 
     echo "Creating backup at $BACKUP_PATH..."
-    # Create a compressed tarball of the relevant directories
     tar -czf "$BACKUP_PATH" arr jellyfin
-
     echo "Backup created: $BACKUP_PATH"
-    
-    # Keep only the 5 most recent backups
-    echo "Cleaning up old backups..."
+
+    echo "Cleaning old backups..."
     find "$BACKUP_DIR" -type f -name "backup_*.tar.gz" | sort -r | sed -e "1,${MAX_BACKUPS}d" | xargs rm -f
 
-    echo "Old backups cleaned. Keeping only the 5 most recent backups."
+    echo "Old backups cleaned."
     find . -name docker-compose.yml -execdir docker compose up -d \;
 }
 
 case "$ACTION" in
   start)
+    healthcheck
     find . -name docker-compose.yml -execdir docker compose up -d \;
     ;;
   stop)
-    find . -name docker-compose.yml -execdir docker compose down \;
+    find . -name docker-compose.yml -execdir docker compose down --remove-orphans \;
     ;;
   update)
     find . -name docker-compose.yml -execdir bash -c 'docker compose down && docker compose pull && docker compose up -d' \;
